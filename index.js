83fbe9b3411c62518d05ac03a5e52d01e0aeeed0
Aizaz <aizazahamd90@gmail.com>
2020-10-12 16:56:18 +0500

    Changed Index.JS

--- index.js
+++ index.js
@@ -11,6 +11,5 @@ app.use(express.json())
 app.use('/books', require('./routes/getBooks'));
 app.use('/books/add', require('./routes/addBook'))
 app.use('/books/update', require('./routes/updateBook'))
-app.use('/books/delete', require('./routes/deleteBook'))
 
 app.listen(PORT, () => console.log(`Server Started at ${PORT}`))
